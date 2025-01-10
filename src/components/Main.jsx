5a96b7fbff27c7e2bb03c15dbb5823e647549869
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-01-10 19:47:28 +0100

    feat: created main component & imported into app component

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+const Main = () => {
+  return (
+    <main className="flex flex-col items-center justify-center w-full gap-5 p-14">
+      <div className="flex flex-col items-center gap-3 text-center">
+        <h1 className="text-3xl font-bold text-Bluebell">Personalized Banners Designed for Your Profession</h1>
+        <p className="text-lg text-Pewter">Transform Your Profile with Stunning AI Banners</p>
+      </div>
+    </main>
+  );
+};
+
+export default Main;
