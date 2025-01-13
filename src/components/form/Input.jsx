ed28f6ba643d68c613a459de4894ea49ba496aff
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-01-14 00:10:15 +0100

    increased width to 100%

--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -1,20 +1,27 @@
 import PropTypes from "prop-types";
 
-const Input = ({ type, placeholder, name }) => {
-  const isText = type === "text";
+const Input = ({ type, placeholder, name, display = "none" }) => {
+  const isNotSocialMedia = name !== "facebook" && name !== "twitter" && name !== "instagram" && name !== "linkedin" && name !== "github" && name !== "behance" && name !== "dribble" && name !== "youtube" && name !== "X" && name !== "website";
 
-  return (
-    <div className={`flex ${isText ? "flex-col" : "flex-row"} items-start gap-1 capitalize`}>
-      {isText && <label htmlFor={name}>{name}</label>}
-      <input type={type} name={name} id={name} placeholder={placeholder} className={isText ? "text-input" : ""} />
-    </div>
-  );
+  if (isNotSocialMedia)
+    return (
+      <div className="flex flex-col items-center w-full gap-1 capitalize sm:w-fit">
+        <label htmlFor={name} className="self-start">
+          {name}
+        </label>
+        <input type={type} name={name} id={name} placeholder={placeholder} className={"text-input"} />
+      </div>
+    );
+
+  return <input type={type} name={name} id={name} placeholder={placeholder} className={"text-input transition-smooth"} style={{ display }} />;
 };
 
 Input.propTypes = {
   type: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
   name: PropTypes.string,
+  width: PropTypes.string,
+  display: PropTypes.string,
 };
 
 export default Input;
