876462f5c1de1db363501552d13e382da1c9f150
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-01-14 00:11:25 +0100

    increased width to 100%

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 
 const Form = ({ children }) => {
-  return <form className="flex flex-col items-center gap-5">{children}</form>;
+  return <form className="flex flex-col items-center w-full gap-5">{children}</form>;
 };
 
 Form.propTypes = {
