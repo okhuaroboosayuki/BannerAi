7ed15c6fd753c83531b430d1b4afff7b2f12590c
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-01-15 02:53:37 +0100

    changed all inputs to controlled inputs

--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 
-const Input = ({ type, placeholder, name, display = "none" }) => {
+const Input = ({ type, placeholder, name, value, onChange, dispatch }) => {
   const isNotSocialMedia = name !== "facebook" && name !== "twitter" && name !== "instagram" && name !== "linkedin" && name !== "github" && name !== "behance" && name !== "dribble" && name !== "youtube" && name !== "X" && name !== "website";
 
   if (isNotSocialMedia)
@@ -9,19 +9,29 @@ const Input = ({ type, placeholder, name, display = "none" }) => {
         <label htmlFor={name} className="self-start">
           {name}
         </label>
-        <input type={type} name={name} id={name} placeholder={placeholder} className={"text-input"} />
+        <input type={type} name={name} id={name} placeholder={placeholder} className={"text-input"} onChange={onChange} value={value} />
       </div>
     );
 
-  return <input type={type} name={name} id={name} placeholder={placeholder} className={"text-input transition-smooth"} style={{ display }} />;
+  return (
+    <div className="flex items-center gap-2">
+      <input type={type} name={name} id={name} placeholder={placeholder} onChange={onChange} className={"scoialMedia-input transition-smooth"} value={value} />
+
+      <span className="px-3 py-[9.5px] capitalize rounded-md bg-Whisper text-Pewter hover:text-black transition-smooth border cursor-pointer" onClick={() => dispatch({ type: "addSocialMedia" })}>
+        add
+      </span>
+    </div>
+  );
 };
 
 Input.propTypes = {
   type: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
   name: PropTypes.string,
-  width: PropTypes.string,
-  display: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  socialButtonClicked: PropTypes.bool,
+  dispatch: PropTypes.func,
 };
 
 export default Input;
