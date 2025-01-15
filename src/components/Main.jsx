73edd0c688fd6e96b877187ecc687a39079b96a2
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-01-15 03:01:24 +0100

    utilised custom useReducer hook, & changed the format for using the SMF component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,14 @@
+import useFormReducer from "../reducer/useFormReducer.jsx";
+import Button from "./Button";
 import Form from "./form/Form";
 import Input from "./form/Input";
 import SocialMediaField from "./form/SocialMediaField";
 
 const Main = () => {
+  const { state, dispatch } = useFormReducer();
+
+  const { name, email, profession, socialMedia, socialMediaLink, socialButtonClicked, socialMediaName } = state;
+
   return (
     <main className="flex flex-col items-center justify-center w-full gap-9 sm:p-14 p-11">
       <div className="flex flex-col items-center gap-3 text-center">
@@ -10,28 +16,37 @@ const Main = () => {
         <p className="text-lg text-Pewter">Transform Your Profile with Stunning Ai Banners</p>
       </div>
 
-      <Form>
-        <Input type={"text"} placeholder={"John Doe"} name={"full name"} />
-        <Input type={"email"} placeholder={"johndoe@email.com"} name={"email"} />
-        <Input type={"text"} placeholder={"Sales Specialist"} name={"profession"} />
+      <Form
+        dispatch={(e) => {
+          e.preventDefault();
+          dispatch({ type: "submit", payload: console.log(name, email, profession, socialMedia, socialMediaLink) });
+        }}>
+        <Input type={"text"} placeholder={"John Doe"} name={"full name"} onChange={(e) => dispatch({ type: "name", payload: e.target.value })} value={name} />
+
+        <Input type={"email"} placeholder={"johndoe@email.com"} name={"email"} onChange={(e) => dispatch({ type: "email", payload: e.target.value })} value={email} />
+
+        <Input type={"text"} placeholder={"Sales Specialist"} name={"profession"} onChange={(e) => dispatch({ type: "profession", payload: e.target.value })} value={profession} />
 
         <div className="grid grid-cols-2 gap-3 px-3 sm:grid-cols-4 h-[200px] sm:h-fit overflow-y-scroll scrollbar-hide">
-          <SocialMediaField platform={"facebook"} />
-          <SocialMediaField platform={"linkedin"} />
-          <SocialMediaField platform={"instagram"} />
-          <SocialMediaField platform={"X"} />
-          <SocialMediaField platform={"github"} />
-          <SocialMediaField platform={"behance"} />
-          <SocialMediaField platform={"dribble"} />
-          <SocialMediaField platform={"youtube"} />
-          <SocialMediaField platform={"website"} />
+          {["facebook", "linkedin", "instagram", "X", "github", "behance", "dribble", "youtube", "website"].map((platform) => (
+            <SocialMediaField key={platform} platform={platform} socialMedia={socialMedia} dispatch={dispatch} socialMediaName={socialMediaName} />
+          ))}
         </div>
 
-        <Input type={"text"} placeholder={"Enter Username"} width="325px" name={"X"} />
+        {socialButtonClicked && (
+          <Input
+            type={"text"}
+            placeholder={socialMediaName !== "website" ? `${socialMediaName} username` : `${socialMediaName} url`}
+            width="325px"
+            name={socialMediaName}
+            onChange={(e) => dispatch({ type: "socialMediaLink", payload: e.target.value })}
+            dispatch={dispatch}
+            value={socialMediaLink}
+            key={socialMediaName}
+          />
+        )}
 
-        <button type="submit" className="button">
-          generate banner
-        </button>
+        <Button text={"generate banner"} />
       </Form>
     </main>
   );
