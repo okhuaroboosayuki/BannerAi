4b930f7649c4eea3c967e7db0d74d6bf2412aad1
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-01-15 02:56:06 +0100

    added onSubmit listener

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,10 +1,15 @@
 import PropTypes from "prop-types";
 
-const Form = ({ children }) => {
-  return <form className="flex flex-col items-center w-full gap-5">{children}</form>;
+const Form = ({ dispatch, children }) => {
+  return (
+    <form className="flex flex-col items-center w-full gap-5" onSubmit={dispatch}>
+      {children}
+    </form>
+  );
 };
 
 Form.propTypes = {
   children: PropTypes.node.isRequired,
+  dispatch: PropTypes.func,
 };
 export default Form;
