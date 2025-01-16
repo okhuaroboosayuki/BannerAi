7c7a863b2d44c1f4a5efa1d82bd19ee66e2b1069
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-01-17 00:07:06 +0100

    feat: added loading state to button, created submit event handler

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,6 @@
 import useFormReducer from "../reducer/useFormReducer.jsx";
+import { handleInputChange } from "../utils/handleInputChange .jsx";
+import { handleValidation } from "../utils/handleValidation.jsx";
 import Button from "./Button";
 import Form from "./form/Form";
 import Input from "./form/Input";
@@ -7,7 +9,35 @@ import SocialMediaField from "./form/SocialMediaField";
 const Main = () => {
   const { state, dispatch } = useFormReducer();
 
-  const { name, email, profession, socialMedia, socialMediaLink, socialButtonClicked, socialMediaName } = state;
+  const { isLoading, name, email, profession, socialMedia, socialMediaLink, socialButtonClicked, socialMediaName, errors } = state;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    handleValidation(state, dispatch);
+
+    const hasErrors = Object.values(state.errors).some((error) => error);
+
+    if (!hasErrors) {
+      dispatch({ type: "loading" });
+
+      setTimeout(() => {
+        // Complete submission and reset state
+
+        const formFilled = name && email && profession && socialMedia.length > 0;
+
+        if (formFilled) {
+          // Submit the form
+          console.log(name, email, profession, socialMedia, socialMediaLink, isLoading);
+          dispatch({ type: "submit" });
+        } else {
+          console.log(name, email, profession, socialMedia, socialMediaLink, isLoading);
+          console.error("Form is incomplete");
+          dispatch({ type: "loading", payload: false });
+        }
+      }, 2000);
+    }
+  };
 
   return (
     <main className="flex flex-col items-center justify-center w-full gap-9 sm:p-14 p-11">
@@ -16,16 +46,12 @@ const Main = () => {
         <p className="text-lg text-Pewter">Transform Your Profile with Stunning Ai Banners</p>
       </div>
 
-      <Form
-        dispatch={(e) => {
-          e.preventDefault();
-          dispatch({ type: "submit", payload: console.log(name, email, profession, socialMedia, socialMediaLink) });
-        }}>
-        <Input type={"text"} placeholder={"John Doe"} name={"full name"} onChange={(e) => dispatch({ type: "name", payload: e.target.value })} value={name} />
+      <Form dispatch={handleSubmit}>
+        <Input type={"text"} placeholder={"John Doe"} name={"full name"} onChange={(e) => handleInputChange("name", e.target.value, errors, dispatch)} value={name} errors={errors} />
 
-        <Input type={"email"} placeholder={"johndoe@email.com"} name={"email"} onChange={(e) => dispatch({ type: "email", payload: e.target.value })} value={email} />
+        <Input type={"email"} placeholder={"johndoe@email.com"} name={"email"} onChange={(e) => handleInputChange("email", e.target.value, errors, dispatch)} value={email} errors={errors} />
 
-        <Input type={"text"} placeholder={"Sales Specialist"} name={"profession"} onChange={(e) => dispatch({ type: "profession", payload: e.target.value })} value={profession} />
+        <Input type={"text"} placeholder={"Sales Specialist"} name={"profession"} onChange={(e) => handleInputChange("profession", e.target.value, errors, dispatch)} value={profession} errors={errors} />
 
         <div className="grid grid-cols-2 gap-3 px-3 sm:grid-cols-4 h-[200px] sm:h-fit overflow-y-scroll scrollbar-hide">
           {["facebook", "linkedin", "instagram", "X", "github", "behance", "dribble", "youtube", "website"].map((platform) => (
@@ -39,14 +65,14 @@ const Main = () => {
             placeholder={socialMediaName !== "website" ? `${socialMediaName} username` : `${socialMediaName} url`}
             width="325px"
             name={socialMediaName}
-            onChange={(e) => dispatch({ type: "socialMediaLink", payload: e.target.value })}
+            onChange={(e) => handleInputChange("socialMediaLink", e.target.value, errors, dispatch)}
             dispatch={dispatch}
             value={socialMediaLink}
             key={socialMediaName}
           />
         )}
 
-        <Button text={"generate banner"} />
+        <Button text={"generate banner"} isLoading={isLoading} />
       </Form>
     </main>
   );
