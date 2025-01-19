07ef9ea05bb6f8b1ebbe4614dd9f4f20f009d4c3
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-01-19 01:05:36 +0100

    made some props required; formatted code with prettier

--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -1,33 +1,51 @@
 import PropTypes from "prop-types";
 
-const Input = ({ type, placeholder, name, value, onChange, dispatch, errors }) => {
-  const isNotSocialMedia = name !== "facebook" && name !== "twitter" && name !== "instagram" && name !== "linkedin" && name !== "github" && name !== "behance" && name !== "dribble" && name !== "youtube" && name !== "X" && name !== "website";
+const Input = ({ type, placeholder, name, value, socialMediaLink, onChange, dispatch, errors }) => {
+  const isSocialMedia = ["facebook", "linkedin", "instagram", "X", "github", "behance", "dribble", "youtube", "website"].includes(name);
 
   const displayNameToKey = {
     "full name": "name",
     email: "email",
     profession: "profession",
-    "social media": "socialMedia",
   };
 
   const errorKey = displayNameToKey[name];
 
-  if (isNotSocialMedia)
+  if (!isSocialMedia)
     return (
-      <div className="flex flex-col items-center w-full gap-1 capitalize sm:w-fit">
-        <label htmlFor={name} className="self-start">
+      <div className="flex flex-col items-center w-full gap-1 sm:w-fit">
+        <label htmlFor={name} className="self-start capitalize">
           {name}
         </label>
-        <input type={type} name={name} id={name} placeholder={placeholder} className={errors[errorKey] ? "text-input border-red-600" : "border-Silvermist text-input"} onChange={onChange} value={value} />
+
+        <input
+          type={type}
+          name={name}
+          id={name}
+          placeholder={placeholder}
+          className={errors[errorKey] ? "text-input border-red-600" : "border-Silvermist text-input"}
+          onChange={onChange}
+          value={value}
+        />
         {errors[errorKey] && <p className="error">{errors[errorKey]}</p>}
       </div>
     );
 
   return (
     <div className="flex items-center gap-2">
-      <input type={type} name={name} id={name} placeholder={placeholder} onChange={onChange} className={"scoialMedia-input transition-smooth"} value={value} />
+      <input
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        onChange={onChange}
+        className={socialMediaLink ? "socialMedia-input border-Silvermist transition-smooth" : "socialMedia-input border-red-600"}
+        value={value}
+      />
 
-      <span className="px-3 py-[9.5px] capitalize rounded-md bg-Whisper text-Pewter hover:text-black transition-smooth border cursor-pointer" onClick={() => dispatch({ type: "addSocialMedia" })}>
+      <span
+        className={`px-3 py-[9.5px] capitalize rounded-md bg-Whisper text-Pewter hover:text-black transition-smooth border cursor-pointer`}
+        onClick={() => socialMediaLink && dispatch({ type: "addSocialMedia" })}>
         add
       </span>
     </div>
@@ -36,11 +54,11 @@ const Input = ({ type, placeholder, name, value, onChange, dispatch, errors }) =
 
 Input.propTypes = {
   type: PropTypes.string.isRequired,
-  placeholder: PropTypes.string,
-  name: PropTypes.string,
-  value: PropTypes.string,
-  onChange: PropTypes.func,
-  socialButtonClicked: PropTypes.bool,
+  placeholder: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  socialMediaLink: PropTypes.string,
   dispatch: PropTypes.func,
   errors: PropTypes.object,
 };
