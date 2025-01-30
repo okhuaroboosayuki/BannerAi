ec061310a68f6e8ee285c8dea1f1545085f5c1ba
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-01-30 17:24:41 +0100

    made the position relative

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Main from "./components/Main";
 
 function App() {
   return (
-    <>
+    <section className="relative">
       <Header />
       <Main />
-    </>
+    </section>
   );
 }
 
