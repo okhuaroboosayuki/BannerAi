50c1aaba8658553ce26e186bcc8099d733f68252
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-01-30 20:17:10 +0100

    changed the way the button click action is handled

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,19 @@
 import { PropTypes } from "prop-types";
 
-const Button = ({ text, isLoading }) => {
+const Button = ({ type, text, className, isLoading, onClick }) => {
   return (
-    <button type="submit" className="button">
+    <button type={type} className={className} onClick={onClick} disabled={isLoading}>
       {isLoading ? "loading..." : text}
     </button>
   );
 };
 
 Button.propTypes = {
+  type: PropTypes.string.isRequired,
+  className: PropTypes.string,
   text: PropTypes.string.isRequired,
   isLoading: PropTypes.bool,
+  onClick: PropTypes.func,
 };
 
 export default Button;
