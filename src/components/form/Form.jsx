25c97bae06afa5f9b4102d34859117680ede00c3
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-01-30 17:25:47 +0100

    added full width to form

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const Form = ({ dispatch, children }) => {
   return (
-    <form className="flex flex-col items-center w-full gap-5" onSubmit={dispatch}>
+    <form className="flex flex-col items-center w-full h-full gap-5" onSubmit={dispatch}>
       {children}
     </form>
   );
