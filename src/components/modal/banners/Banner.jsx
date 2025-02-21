32c8816eaa5f3716d9a81f941cca1b3039cc3100
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-02-21 21:50:04 +0100

    refactor Banner component to use forwardRef and improve layout structure

--- src/components/modal/banners/Banner.jsx
+++ src/components/modal/banners/Banner.jsx
@@ -1,11 +1,9 @@
 import PropTypes from "prop-types";
-// import { createElement } from "react";
-// import { createRoot } from "react-dom/client";
+import { socialIcons, otherIcons } from "../../../assets/icons";
+import emailIcon from "../../../assets/icons/icons8-email-50.png";
+import { forwardRef, useEffect } from "react";
 
-import { socialIcons } from "../../../assets/icons";
-import { useEffect } from "react";
-
-const Banner = ({ name, email, profession, socialMedia, generatedOutput }) => {
+const Banner = forwardRef(({ name, email, profession, socialMedia, generatedOutput }, ref) => {
   const getIcon = (platform) => {
     const iconEntry = socialIcons.find((icon) => icon.platform === platform);
     return iconEntry ? iconEntry.icon : null;
@@ -21,9 +19,6 @@ const Banner = ({ name, email, profession, socialMedia, generatedOutput }) => {
     if (generatedOutput && generatedOutput[0]?.fontFamily.link) {
       // Extract the link string and parse it
       const linkString = generatedOutput[0]?.fontFamily.link;
-      // const parser = new DOMParser();
-      // const doc = parser.parseFromString(linkString, "text/html");
-      // const linkHref = doc.querySelector("link").getAttribute("href");
 
       // Create a new link element
       const link = document.createElement("link");
@@ -42,7 +37,7 @@ const Banner = ({ name, email, profession, socialMedia, generatedOutput }) => {
 
   return (
     <div className="w-full p-5 bg-white/50 h-[300px]">
-      <div className="flex self-center w-full h-full border transition-smooth" style={{ backgroundColor: generatedOutput[0].colors.generalBgColor }}>
+      <div className="flex self-center w-full h-full border transition-smooth" style={{ backgroundColor: generatedOutput[0].colors.generalBgColor }} ref={ref}>
         {/* social media area */}
         <div className="flex flex-col items-start justify-center w-1/4 gap-3 px-8 py-5" style={{ backgroundColor: generatedOutput[0].colors.socialMediaBgColor }}>
           {socialMediaWithoutWebSite.map((entry, index) => {
@@ -64,8 +59,8 @@ const Banner = ({ name, email, profession, socialMedia, generatedOutput }) => {
         </div>
 
         <div className="flex justify-between flex-1 w-full px-5 py-5" style={{ fontFamily: generatedOutput[0].fontFamily.declaration }}>
-          {/* name and profession area */}
-          <div className="flex flex-col items-start justify-between flex-1 w-full">
+          <div className="flex flex-col items-start justify-between w-full h-full gap-2">
+            {/* name and profession area */}
             <div className="flex flex-col items-start gap-1">
               <h1
                 className="font-extrabold capitalize"
@@ -80,6 +75,39 @@ const Banner = ({ name, email, profession, socialMedia, generatedOutput }) => {
               </p>
             </div>
 
+            {/* email and social media area */}
+            <div className="flex items-center justify-between h-full gap-3 text-sm">
+              <div className="flex items-center justify-center gap-1 text-sm" style={{ color: generatedOutput[0].colors.textColor }}>
+                <div className="p-[3px] bg-transparent rounded-md">
+                  <img src={emailIcon} alt="email icon" width={15} height={15} />
+                </div>
+
+                <span style={{ fontFamily: generatedOutput[0].fontFamily.declaration }}>{email}</span>
+              </div>
+
+              {socialMediaWithWebSite &&
+                socialMediaWithWebSite?.map((website, index) => {
+                  const platform = Object.keys(website)[0];
+                  const url = website[platform];
+                  const icon = getIcon(platform);
+                  return (
+                    <div
+                      key={index}
+                      style={{ fontFamily: generatedOutput[0].fontFamily.declaration, fontSize: generatedOutput[0].fontSize.textFontSize, color: generatedOutput[0].colors.textColor }}
+                      className="flex items-center gap-1">
+                      {icon && (
+                        <div className="p-[3px] bg-white rounded-md">
+                          <img src={icon} alt={`${platform}'s icon`} height={15} width={15} />
+                        </div>
+                      )}
+                      <span className="text-sm" style={{ fontFamily: generatedOutput[0].fontFamily.declaration }}>
+                        {url}
+                      </span>
+                    </div>
+                  );
+                })}
+            </div>
+
             {/* quote area */}
             <div className="flex items-center justify-center">
               <em
@@ -90,34 +118,25 @@ const Banner = ({ name, email, profession, socialMedia, generatedOutput }) => {
                 }}>
                 {generatedOutput[0].quote.text}
               </em>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 100 24" strokeWidth={2} stroke="currentColor" className="w-20 h-4 ml-2">
-                <line x1="0" y1="12" x2="80" y2="12" stroke="currentColor" strokeWidth="2" />
-                <polyline points="80,6 90,12 80,18" fill="none" stroke="currentColor" strokeWidth="2" />
-              </svg>
             </div>
           </div>
 
-          {/* email and social media area */}
-          <div className="flex flex-col items-start h-full justify-between gap-3">
-            <div className="flex items-center justify-center gap-3 text-sm" style={{ color: generatedOutput[0].colors.textColor }}>
-              <span style={{ fontFamily: generatedOutput[0].fontFamily.declaration, fontSize: generatedOutput[0].fontSize.textFontSize }}>{email}</span>
-            </div>
-
-            {socialMediaWithWebSite?.map((website, index) => {
-              const platform = Object.keys(website)[0];
-              const url = website[platform];
-              return (
-                <p key={index} style={{ fontFamily: generatedOutput[0].fontFamily.declaration, fontSize: generatedOutput[0].fontSize.textFontSize }}>
-                  {url}
-                </p>
-              );
-            })}
+          <div className="flex flex-col items-center justify-center w-full gap-4">
+            {otherIcons
+              .sort(() => Math.random() - 0.5)
+              .map((icon) => (
+                <div key={icon.name} className={`flex w-full items-center ${icon.name === "hashtag" || icon.name === "mail" || icon.name === "splash" ? "justify-center" : "justify-end"}`}>
+                  <img src={icon.icon} alt={icon.name} width={20} height={20} />
+                </div>
+              ))}
           </div>
         </div>
       </div>
     </div>
   );
-};
+});
+
+Banner.displayName = "Banner";
 
 Banner.propTypes = {
   name: PropTypes.string,
@@ -149,23 +168,5 @@ Banner.propTypes = {
     })
   ).isRequired,
 };
-
-// const Banner = ({ generatedOutput }) => {
-//   const convertToNode = (string) => {
-//     const template = document.createElement("template");
-//     template.innerHTML = string.trim(); // Parses the string as HTML
-//     return Array.from(template.content.childNodes).map((node, index) => createElement(node.nodeName.toLowerCase(), { key: index }, node.textContent || node.innerHTML));
-//   };
-
-//   return (
-//     <div className="w-full p-5 bg-white/50 h-[300px]" id="banner">
-//       {convertToNode(generatedOutput)}
-//     </div>
-//   );
-// };
-
-// Banner.propTypes = {
-//   generatedOutput: PropTypes.string.isRequired,
-// };
 
 export default Banner;
