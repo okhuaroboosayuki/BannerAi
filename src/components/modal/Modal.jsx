b3d531f36439f6f028f1dbaf1cd2adb819dd6800
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-02-21 21:49:06 +0100

    update Modal component to use full height for better layout consistency

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 
 const Modal = ({ children }) => {
-  return <div className="absolute top-0 left-0 z-50 flex flex-col items-center w-full min-h-screen py-20 justify-evenly px-60 bg-Silvermist/90">{children}</div>;
+  return <div className="absolute top-0 left-0 z-50 flex flex-col items-center w-full h-full py-20 justify-evenly px-60 bg-Silvermist/90">{children}</div>;
 };
 
 Modal.propTypes = {
