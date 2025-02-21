dda053c2e652cbb115f1f8b50ef87157eee386a7
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-02-21 21:47:56 +0100

    added disabled prop to Button component to control button state

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import { PropTypes } from "prop-types";
 
-const Button = ({ type, text, className, isLoading, onClick }) => {
+const Button = ({ type, text, className, isLoading, onClick, disabled }) => {
   return (
-    <button type={type} className={className} onClick={onClick} disabled={isLoading}>
+    <button type={type} className={className} onClick={onClick} disabled={disabled}>
       {isLoading ? "loading..." : text}
     </button>
   );
@@ -13,6 +13,7 @@ Button.propTypes = {
   className: PropTypes.string,
   text: PropTypes.string.isRequired,
   isLoading: PropTypes.bool,
+  disabled: PropTypes.any,
   onClick: PropTypes.func,
 };
 
