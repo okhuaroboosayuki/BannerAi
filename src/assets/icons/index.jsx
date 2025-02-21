d1d1d4b0980d121ff73929d5f74ea0c04f125a6d
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-02-21 21:46:49 +0100

    added social media icons and updated icon index

---
+++ src/assets/icons/index.jsx
@@ -0,0 +1,76 @@
+import facebookIcon from "./icons8-facebook.svg";
+import behanceIcon from "./icons8-behance.svg";
+import githubIcon from "./icons8-github.svg";
+import instagramIcon from "./icons8-instagram.svg";
+import linkedinIcon from "./icons8-linkedin.svg";
+import websiteIcon from "./icons8-website-51.png";
+import xIcon from "./icons8-x.svg";
+import youtubeIcon from "./icons8-youtube.svg";
+import dribbleIcon from "./icons8-dribble.svg";
+import hashtag from "./icons8-hashtag-96.png";
+import purpleAt from "./icons8-@-96-purple.png";
+import mailIcon from "./icons8-mail-100-purple.png";
+import messageIcon from "./icons8-messages-96-purple.png";
+import splashIcon from "./icons8-splash-96-purple.png";
+
+export const socialIcons = [
+  {
+    platform: "facebook",
+    icon: facebookIcon,
+  },
+  {
+    platform: "behance",
+    icon: behanceIcon,
+  },
+  {
+    platform: "github",
+    icon: githubIcon,
+  },
+  {
+    platform: "instagram",
+    icon: instagramIcon,
+  },
+  {
+    platform: "linkedin",
+    icon: linkedinIcon,
+  },
+  {
+    platform: "website",
+    icon: websiteIcon,
+  },
+  {
+    platform: "X (twitter)",
+    icon: xIcon,
+  },
+  {
+    platform: "youtube",
+    icon: youtubeIcon,
+  },
+  {
+    platform: "dribble",
+    icon: dribbleIcon,
+  },
+];
+
+export const otherIcons = [
+  {
+    name: "hashtag",
+    icon: hashtag,
+  },
+  {
+    name: "at",
+    icon: purpleAt,
+  },
+  {
+    name: "mail",
+    icon: mailIcon,
+  },
+  {
+    name: "messages",
+    icon: messageIcon,
+  },
+  {
+    name: "splash",
+    icon: splashIcon,
+  },
+];
