0a03a927b88c1388c1d45ea11b652bc65391b3e1
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-02-25 09:52:22 +0100

    add index files for form components and utility functions

---
+++ src/utils/index.jsx
@@ -0,0 +1,5 @@
+import { handleInputChange } from "./handleInputChange ";
+import { handleValidation } from "./handleValidation";
+import { validateEmail } from "./validateEmail";
+
+export { handleInputChange, handleValidation, validateEmail };
