26acb1ff514fb4a15b5d1768b162df390dd95f86
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-02-25 20:16:23 +0100

    enhance Modal component layout for improved responsiveness

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,7 +1,9 @@
 import PropTypes from "prop-types";
 
 const Modal = ({ children }) => {
-  return <div className="absolute top-0 left-0 z-50 flex flex-col items-center w-full h-full py-20 justify-evenly px-60 bg-Silvermist/90">{children}</div>;
+  return (
+    <div className="absolute top-0 left-0 z-50 flex flex-col items-center w-full h-screen py-5 md:h-full lg:py-10 md:px-8 lg:px-28 xl:py-20 justify-evenly xl:px-60 bg-Silvermist/90">{children}</div>
+  );
 };
 
 Modal.propTypes = {
