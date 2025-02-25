be6a961241e0a52644d64e66e8005c02cf001c21
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-02-25 10:17:54 +0100

    add Modal and Form components to index file, replacing Main export

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,7 @@
 export { default as Header } from "./Header";
 export { default as Button } from "./Button";
-export { default as Main } from "./Main";
+export { default as Modal } from "./modal/Modal";
+export { default as SimpleBanner } from "./modal/banners/SimpleBanner";
+export { default as Form } from "./form/Form";
+export { default as Input } from "./form/Input";
+export { default as SocialMediaField } from "./form/SocialMediaField";
