ddf57276836bbab04e3c3a6fcc2f034a8d9679b3
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-02-26 02:00:37 +0100

    fix Modal component layout to ensure full height on smaller screens

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const Modal = ({ children }) => {
   return (
-    <div className="absolute top-0 left-0 z-50 flex flex-col items-center w-full h-screen py-5 md:h-full lg:py-10 md:px-8 lg:px-28 xl:py-20 justify-evenly xl:px-60 bg-Silvermist/90">{children}</div>
+    <div className="absolute top-0 left-0 z-50 flex flex-col items-center w-full py-5 h-full sm:h-screen lg:py-10 md:px-8 lg:px-28 xl:py-20 justify-evenly xl:px-60 bg-Silvermist/90">{children}</div>
   );
 };
 
