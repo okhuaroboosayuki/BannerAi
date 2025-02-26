e08917b7ad4435c5a98c8b7ad9c2367dd5097913
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-02-26 02:00:58 +0100

    implement scroll-based header visibility toggle for improved user experience

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,35 @@
+import { useEffect, useRef } from "react";
 import logo from "../assets/icons/bannerAi_96x96.png";
 import useFormReducer from "../hooks/useFormReducer";
 
 const Header = () => {
-  const { state, dispatch } = useFormReducer();
+  const headerRef = useRef(null);
 
-  const { generatedOutput } = state;
+  const { state } = useFormReducer();
+  const { openModal } = state;
+
+  useEffect(() => {
+    const header = headerRef.current;
+
+    let lastScrollY = window.scrollY;
+
+    window.addEventListener("scroll", () => {
+      if (openModal) return header.classList.add("header-hidden");
+
+      if (!openModal) {
+        window.scrollY > lastScrollY ? header.classList.add("header-hidden") : header.classList.remove("header-hidden");
+        lastScrollY = window.scrollY;
+      }
+    });
+  }, [openModal]);
 
   return (
-    <header className="w-full p-4 border-b-[0.5px] border-b-grey flex justify-between items-center">
-      <div className="flex items-center justify-center">
+    <header className="fixed-header" ref={headerRef}>
+      <div className="flex items-center justify-center ml-8">
         <img src={logo} alt="logo icon" width={50} height={50} />
         <span className="text-3xl cursor-default font-cabin-sketch">BannerAi</span>
         {/* <span className="text-3xl cursor-default font-cabin-sketch">BannerCraft</span> */}
       </div>
-
-      {generatedOutput && (
-        <button onClick={() => dispatch({ type: "modal", payload: true })} className="capitalize">
-          view banner
-        </button>
-      )}
     </header>
   );
 };
