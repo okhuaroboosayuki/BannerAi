f885854c6f30efe8b48fb2bf3b0caefc82f55a28
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-02-27 22:26:43 +0100

    refactor Header component to remove unused scroll handling and update styles for improved layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,8 @@
-import { useEffect, useRef } from "react";
 import logo from "../assets/icons/bannerAi_96x96.png";
-import useFormReducer from "../hooks/useFormReducer";
 
 const Header = () => {
-  const headerRef = useRef(null);
-
-  const { state } = useFormReducer();
-  const { openModal } = state;
-
-  useEffect(() => {
-    const header = headerRef.current;
-
-    let lastScrollY = window.scrollY;
-
-    window.addEventListener("scroll", () => {
-      if (openModal) return header.classList.add("header-hidden");
-
-      if (!openModal) {
-        window.scrollY > lastScrollY ? header.classList.add("header-hidden") : header.classList.remove("header-hidden");
-        lastScrollY = window.scrollY;
-      }
-    });
-  }, [openModal]);
-
   return (
-    <header className="fixed-header" ref={headerRef}>
+    <header className="w-full p-4 border-b-[0.5px] border-b-gray-200 flex justify-between items-center">
       <div className="flex items-center justify-center ml-8">
         <img src={logo} alt="logo icon" width={50} height={50} />
         <span className="text-3xl cursor-default font-cabin-sketch">BannerAi</span>
