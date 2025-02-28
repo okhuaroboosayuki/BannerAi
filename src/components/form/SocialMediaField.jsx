aa9514bd4daab622ef807ade13fc684f6fc2eb05
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-02-28 01:28:45 +0100

    refactor SocialMediaField component to improve editable state handling and update styling for disabled states

--- src/components/form/SocialMediaField.jsx
+++ src/components/form/SocialMediaField.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import { toast } from "react-toastify";
 
-const SocialMediaField = ({ platform, dispatch, socialMediaName, socialMedia, generatedOutput }) => {
+const SocialMediaField = ({ platform, dispatch, socialMediaName, socialMedia, editable }) => {
   const isSameName = socialMediaName === platform;
   const hasPlatformKey = socialMedia.some((item) => Object.keys(item).includes(platform)); // Check if platform key exists
   const platformHasValue = socialMedia.some((item) => item[platform]); // Check if platform has a value
@@ -14,33 +14,31 @@ const SocialMediaField = ({ platform, dispatch, socialMediaName, socialMedia, ge
       return;
     }
 
-    if (generatedOutput) return;
+    if (!editable) return;
 
     dispatch({ type: "socialButtonClicked", payload: platform });
   };
 
   return (
     <div
-      className={`flex items-center w-full sm:w-[116px] justify-center gap-2 px-[5px] py-[10.5px] ${
-        hasPlatformKey
-          ? platformHasValue !== "" // Check if the value is NOT empty
-            ? "border border-Bluebell" // If it's not empty, add a border with blue color
-            : "bg-Whisper buttonWithSVG-hover" // If it's empty, add a background color of Whisper
-          : `bg-Whisper ${socialMediaEqualToFive && !hasPlatformKey ? "cursor-not-allowed" : "cursor-pointer buttonWithSVG-hover"}` // If the platform key doesn't exist, add a background color of Whisper
-      } rounded-md `}
+      className={`flex items-center w-full sm:w-[116px] justify-center gap-2 px-[5px] py-[10.5px] rounded-md ${
+        hasPlatformKey && platformHasValue !== "" // Check if platform has a value and key exists to determine the color of the button
+          ? `border border-Bluebell hover:border-DeepBlue ${!editable ? "cursor-not-allowed bg-gray-300" : "cursor-pointer bg-Whisper"}`
+          : `${!editable ? "bg-gray-400 border-gray-300 cursor-not-allowed" : "bg-gray-200 cursor-pointer buttonWithSVG-hover"}`
+      }`}
       onClick={() => handleSocialMediaClick()}
       role="button"
       tabIndex="0">
       {!isSameName ? (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="15px" height="15px" fillRule="evenodd" fill={hasPlatformKey ? "blue" : "#565E6C"}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="15px" height="15px" fillRule="evenodd" fill={hasPlatformKey ? "#5068E2" : "#565E6C"}>
           <path fillRule="evenodd" d="M 11 2 L 11 11 L 2 11 L 2 13 L 11 13 L 11 22 L 13 22 L 13 13 L 22 13 L 22 11 L 13 11 L 13 2 Z" />
         </svg>
       ) : (
-        <svg xmlns="http://www.w3.org/2000/svg" height="15px" viewBox="0 -960 960 960" width="15px" fill="#565E6C">
+        <svg xmlns="http://www.w3.org/2000/svg" height="15px" viewBox="0 -960 960 960" width="15px" fill={hasPlatformKey ? "#5068E2" : "#565E6C"}>
           <path d="M200-440v-80h560v80H200Z" />
         </svg>
       )}
-      <span className={hasPlatformKey ? "text-Bluebell" : "text-Pewter"}>{platform}</span>
+      <span className={hasPlatformKey ? "text-Bluebell" : `${!editable ? "text-gray-700" : "text-Pewter"}`}>{platform}</span>
     </div>
   );
 };
@@ -50,7 +48,7 @@ SocialMediaField.propTypes = {
   dispatch: PropTypes.func,
   socialMediaName: PropTypes.string,
   socialMedia: PropTypes.array,
-  generatedOutput: PropTypes.any,
+  editable: PropTypes.bool,
 };
 
 export default SocialMediaField;
