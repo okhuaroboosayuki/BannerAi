59fe14d17dd84a57449dcacc420e6b2e25a455db
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-03-09 23:06:34 +0100

    feat: add new social media icons with white backgrounds and arrow right icon

--- src/assets/icons/index.jsx
+++ src/assets/icons/index.jsx
@@ -1,17 +1,26 @@
 import facebookIcon from "./icons8-facebook.svg";
+import facebookIconWhiteBg from "./icons8-facebook-white-bg.svg";
 import behanceIcon from "./icons8-behance.svg";
 import githubIcon from "./icons8-github.svg";
 import instagramIcon from "./icons8-instagram.svg";
 import linkedinIcon from "./icons8-linkedin.svg";
+import behanceIconWhiteBg from "./icons8-behance-white-bg.svg";
+import githubIconWhiteBg from "./icons8-github-white-bg.svg";
+import instagramIconWhiteBg from "./icons8-instagram-white-bg.svg";
+import linkedinIconWhiteBg from "./icons8-linkedin-white-bg.svg";
 import websiteIcon from "./icons8-website-51.png";
 import xIcon from "./icons8-x.svg";
 import youtubeIcon from "./icons8-youtube.svg";
 import dribbleIcon from "./icons8-dribble.svg";
+import xIconWhiteBg from "./icons8-x-white-bg.svg";
+import youtubeIconWhiteBg from "./icons8-youtube-white-bg.svg";
+import dribbleIconWhiteBg from "./icons8-dribble-white-bg.svg";
 import hashtag from "./icons8-hashtag-96.png";
 import purpleAt from "./icons8-@-96-purple.png";
 import mailIcon from "./icons8-mail-100-purple.png";
 import messageIcon from "./icons8-messages-96-purple.png";
 import splashIcon from "./icons8-splash-96-purple.png";
+import arrowRight from "./icons8-arrow-right-gray.png";
 
 export const socialIcons = [
   {
@@ -52,6 +61,41 @@ export const socialIcons = [
   },
 ];
 
+export const socialIconsWithWhiteBg = [
+  {
+    platform: "facebook",
+    icon: facebookIconWhiteBg,
+  },
+  {
+    platform: "behance",
+    icon: behanceIconWhiteBg,
+  },
+  {
+    platform: "github",
+    icon: githubIconWhiteBg,
+  },
+  {
+    platform: "instagram",
+    icon: instagramIconWhiteBg,
+  },
+  {
+    platform: "linkedin",
+    icon: linkedinIconWhiteBg,
+  },
+  {
+    platform: "X (twitter)",
+    icon: xIconWhiteBg,
+  },
+  {
+    platform: "youtube",
+    icon: youtubeIconWhiteBg,
+  },
+  {
+    platform: "dribble",
+    icon: dribbleIconWhiteBg,
+  },
+];
+
 export const otherIcons = [
   {
     name: "hashtag",
@@ -72,5 +116,9 @@ export const otherIcons = [
   {
     name: "splash",
     icon: splashIcon,
+  },
+  {
+    name: "arrowRight",
+    icon: arrowRight,
   },
 ];
