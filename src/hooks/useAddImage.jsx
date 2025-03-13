eaab98e3650a05827a8b265547bd1dbdd5a8c811
okhuaroboosayuki <osayukiraymond@gmail.com>
2025-03-13 21:03:39 +0100

    feat: enhance useAddImage hook to support image removal from Supabase storage

--- src/hooks/useAddImage.jsx
+++ src/hooks/useAddImage.jsx
@@ -1,33 +1,50 @@
-import { truncateFilename } from "../utils";
+import supabase from "../services/supabase";
 
-const useAddImage = (imageInputRef, dispatchFn) => {
+const useAddImage = (imageInputRef, dispatchFn, image, imageInputName) => {
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+
+    if (!file) {
+      cleanUp();
+      return;
+    }
+
+    if (file) {
+      dispatchFn({ type: "image", payload: file });
+      dispatchFn({ type: "imageInputName", payload: file.name });
+    }
+  };
+
+  function cleanUp() {
+    imageInputRef.current.value = null;
+    imageInputRef.current.removeEventListener("change", handleFileChange);
+  }
+
   const handleAddImage = () => {
     imageInputRef.current.click();
-
-    const handleFileChange = (e) => {
-      const file = e.target.files[0];
-
-      if (file) {
-        const imageUrl = URL.createObjectURL(file);
-        const shortFileName = truncateFilename(file.name, 15);
-
-        dispatchFn({ type: "image", payload: imageUrl });
-        dispatchFn({ type: "imageInputName", payload: shortFileName });
-      }
-
-      imageInputRef.current.value = null;
-      imageInputRef.current.removeEventListener("change", handleFileChange);
-    };
-
     imageInputRef.current.addEventListener("change", handleFileChange);
   };
 
-  const clearImageInput = () => {
+  const clearImageInput = async () => {
+    const containsWebLink = image instanceof File;
+
+    if (!containsWebLink) {
+      const { error } = await supabase.storage.from("image-store").remove([imageInputName]);
+      console.log("image cleared");
+      if (error) console.log(error);
+
+      dispatchFn({ type: "image", payload: "" });
+      dispatchFn({ type: "imageInputName", payload: "choose image" });
+      cleanUp();
+      return;
+    }
+
     dispatchFn({ type: "image", payload: "" });
     dispatchFn({ type: "imageInputName", payload: "choose image" });
+    cleanUp();
   };
 
-  return { handleAddImage, clearImageInput };
+  return { handleAddImage, clearImageInput, cleanUp };
 };
 
 export default useAddImage;
